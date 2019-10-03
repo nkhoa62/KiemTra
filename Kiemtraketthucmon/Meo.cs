243daad987fc308312569dcc7a30e0f6319580d1
nkhoa62 <nkhoa62@gmail.com>
2019-10-03 14:39:44 +0700

    Bài Tập : bước 4

---
+++ Kiemtraketthucmon/Meo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiemtraketthucmon
+{
+    class Meo: DongVat
+    {
+        public String meo;
+        public void keu()
+        {
+            meo = "Meo meo";
+        }
+    }
+}
